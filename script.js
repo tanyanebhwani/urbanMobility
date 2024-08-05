48a7aa841b72c38fd84ab5114f1d208b5db16e67
tanyanebhwani <92228566+tanyanebhwani@users.noreply.github.com>
2024-08-05 13:31:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ menu = document.getElementsByClassName('nav-list')[0];
 menuBlock = document.getElementsByClassName('nav-items-vertical')[0];
 navBar = document.getElementsByClassName('navbar')[0];
 bannerText = document.getElementsByClassName('banner-text')[0];
+body = document.getElementsByTagName('body')[0];
 rotate = 0;
 svg.addEventListener('click', () => {
     console.log('I am listening');
     if (rotate == 0) {
         svg.style.rotate = '90deg';
         rotate = 1;
-        console.log("rotate = 1");
+        menuBlock.style.display = 'flex';
         menu.style.display = 'block';
         menuBlock.style.height = '150px';
         menuBlock.style.width = "100%";
@@ -19,7 +20,6 @@ svg.addEventListener('click', () => {
     else if (rotate == 1) {
         svg.style.rotate = '0deg';
         rotate = 0;
-        console.log("rotate = 0");
         menuBlock.style.height = '0';
         navBar.style.position = 'absolute';
         bannerText.style.height = '80vh';
@@ -69,4 +69,13 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         }
     });
+    document.getElementsByClassName('btn-signup')[0].addEventListener('click',()=>{
+        window.location.href = '\signup.html';
+    })
+    document.getElementsByClassName('btn-signup')[1].addEventListener('click',()=>{
+        window.location.href = '\signup.html';
+    })
+    document.getElementsByClassName('btn-demo')[0].addEventListener('click',()=>{
+        window.location.href = '\home.html';
+    })
 });
