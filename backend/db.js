9e1c67db21e752a7c62c4dbcf917ccb522414514
Tanya Nebhwani <nebhwanitanya0@gmail.com>
2024-08-10 15:56:08 +0530

    hosting the site

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose');
+/*const mongoose = require('mongoose');
 const mongoURI = "mongodb://127.0.0.1:27017/urbanMobility";
-
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const connectToMongo = async()=>{
   await mongoose.connect(mongoURI, {
     useNewUrlParser: true,
@@ -9,3 +9,4 @@ const connectToMongo = async()=>{
 }
 connectToMongo().catch(err => console.log(err));
 module.exports = connectToMongo;
+*/
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,29 @@
-const connectToMongo = require('./db');
+//const connectToMongo = require('./db');
 const express = require('express');
 var cors = require('cors');
 var dotenv = require('dotenv');
 const authRoutes = require('./routes/auth');
 const favoriteRoutes = require('./routes/favorite');
+const { default: mongoose, Mongoose } = require('mongoose');
 dotenv.config();
+//const port = 5000;
 
-const port = 5000;
+mongoose.connect(
+  `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.di4bo.mongodb.net/urbanMobility`, 
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+)
+.then(() => {
+  console.log("Connected to database");
+})
+.catch((err) => {
+  console.error("Error connecting to database:", err);
+});
 
 const app = express();
-connectToMongo()
-  .then(() => {
-    console.log("Connected to database")});
+/*connectToMongo()*/
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // To parse URL-encoded bodies
@@ -20,6 +32,6 @@ app.use(cors());
 app.use('/api/auth', authRoutes);
 app.use('/api/favorites', favoriteRoutes);
 
-app.listen(port, () => {
+/*app.listen(port, () => {
     console.log(`Urban mobility backend listening on port ${port}`);
-  });
+  });*/
